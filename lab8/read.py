b7e78d34d2811b874b0f9c9e34e9308f9138b7b2
Vladyslav Hirchuk <145594568+SoloScriptSage@users.noreply.github.com>
2023-09-20 13:34:29 -0500

    Create read.py

---
+++ lab8/read.py
@@ -0,0 +1,32 @@
+# Lab 8
+# Hirchuk Vladyslav
+
+def read_info(filename):
+    golf = []
+    with open(filename, 'r') as infile:
+        TITLE = infile.readline().strip()  # Read and store the header line
+        for line in infile:
+            index, data = line.strip().split('. ')
+            name, score = data.split('\t\t')
+            golf.append((name.strip(), score.strip()))
+    return TITLE, golf
+
+name_file = 'golf.txt'
+
+try:
+    TITLE, golf = read_info(name_file)
+
+    print(f"Title: {TITLE}")
+    if len(golf) > 0:
+        print("Golfer's name\t|\tScore")
+        print("===============================")
+        for i, (name, score) in enumerate(golf, start=1):
+            print(f"{i}. {name}\t|\t{score}")
+    else:
+        print("No data found in the file.")
+except FileNotFoundError:
+    print("The 'golf.txt' file does not exist.")
+except Exception as e:
+    print(f"An error occurred: {e}")
+
+print("Done!")
