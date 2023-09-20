568097bdd1b0434b1238022bdd35eeeb67c8cc86
Vladyslav Hirchuk <145594568+SoloScriptSage@users.noreply.github.com>
2023-09-20 13:35:52 -0500

    Create ChessPlayer.py

---
+++ lab6-itp/ChessPlayer.py
@@ -0,0 +1,13 @@
+import databaseio
+
+# Constructor chessplayer
+class ChessPlayer:
+    def __init__(self, Name, Age, Rating, FavoritePiece):
+        self.name = Name
+        self.age = Age
+        self.rating = Rating
+        self.favoritePiece = FavoritePiece
+    
+    def display(self):
+        databaseio.print_data(self.name, self.age, self.rating, self.favoritePiece)
+
